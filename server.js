c7be16ecd70a4dfa96518833f2138717728adff5
ivanisayenko <ivanisayenko@hotmail.es>
2018-04-25 22:33:33 +0200

    el server funiona ok, pero el html no pilla el style

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-app.use('/css',express.static(__dirname + '/code/css'));
-app.use('/js',express.static(__dirname + '/code/js'));
-app.use('/assets',express.static(__dirname + '/media'));
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
+// app.use('/css', express.static(__dirname + '/code'));
+// app.use('/js', express.static(__dirname + '/code'));
+// app.use('/assets', express.static(__dirname + '/media'));
+server.listen(3000, function(){
+  console.log('listening on *:3000');
 });
-io.on('connection', function(){ console.log("Hola") });
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', function (socket) { console.log("usuari connectat") });
 server.listen(3000);
