d1e67d895c97d07b7b3ac8ed1089e0db923eaadc
nacors <nacor71@gmail.com>
2018-04-25 13:20:25 +0200

    server

--- code/js/server.js
+++ code/js/server.js
@@ -1,5 +1,8 @@
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-io.on('connection', function(){ /* … */ });
+app.get('/', function (req, res) {
+    res.render('index', {});
+  });
+io.on('connection', function(){ console.log("Hola") });
 server.listen(3000);
