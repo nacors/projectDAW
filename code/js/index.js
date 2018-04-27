20807e815162a959540964980986d3164c518401
ivanisayenko <ivanisayenko@hotmail.es>
2018-04-27 08:55:41 +0200

    correccion importacion archivos js y css

--- code/js/index.js
+++ code/js/index.js
@@ -1,1 +1,26 @@
-console.log("Hola");
+window.onload = () => {
+    document.getElementById("accionRegistrar").addEventListener("click", function () {
+        this.style.display = "none";
+        document.getElementById("iniciarSesion").style.display = "none";
+        document.getElementById("registrarse").setAttribute("style", "display: grid !important");
+        document.getElementById("accionIniciarSesion").setAttribute("style", "display: inline");
+    });
+    document.getElementsByName("invitado")[0].addEventListener("click", function (e) {
+        e.preventDefault();
+        alert("jugar como invitado (acabar)");
+    });
+    document.getElementsByName("iniciar")[0].addEventListener("click", function (e) {
+        e.preventDefault();
+        alert("jugar como usuario registrado (acabar)");
+    });
+    document.getElementsByName("registrar")[0].addEventListener("click", function (e) {
+        e.preventDefault();
+        alert("jugar como usuario recien registrado (acabar)");
+    });
+    document.getElementById("accionIniciarSesion").addEventListener("click", function () {
+        this.style.display = "none";
+        document.getElementById("iniciarSesion").style.display = "grid";
+        document.getElementById("registrarse").setAttribute("style", "display: none !important");
+        document.getElementById("accionRegistrar").setAttribute("style", "display: inline");
+    });
+}
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-// app.use('/css', express.static(__dirname + '/code'));
-// app.use('/js', express.static(__dirname + '/code'));
-// app.use('/assets', express.static(__dirname + '/media'));
+app.use('/css', express.static(__dirname + '/code/css'));
+app.use('/js', express.static(__dirname + '/code/js'));
+app.use('/assets', express.static(__dirname + '/media'));
 server.listen(3000, function(){
   console.log('listening on *:3000');
 });
