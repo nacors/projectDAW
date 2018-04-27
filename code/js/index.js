9625ffc24b5b8e828582b7bc5db456e02b83a5d5
ivanisayenko <ivanisayenko@hotmail.es>
2018-04-27 14:08:08 +0200

    Creacion de logeo y registro de usuarios

--- code/js/index.js
+++ code/js/index.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    //funciones del lado cliente
     document.getElementById("accionRegistrar").addEventListener("click", function () {
         this.style.display = "none";
         document.getElementById("iniciarSesion").style.display = "none";
@@ -11,12 +12,13 @@ window.onload = () => {
     });
     document.getElementsByName("iniciar")[0].addEventListener("click", function (e) {
         e.preventDefault();
-        alert("jugar como usuario registrado (acabar)");
-        Client.iniciarSesion();
+        console.log();
+        Client.iniciarSesion(document.getElementsByName("usernameI")[0].value, document.getElementsByName("passwordI")[0].value);
     });
     document.getElementsByName("registrar")[0].addEventListener("click", function (e) {
         e.preventDefault();
-        alert("jugar como usuario recien registrado (acabar)");
+        Client.registrarse(document.getElementsByName("usernameR")[0].value, document.getElementsByName("passwordR")[0].value);
+        console.log("usuario registrado con exito");
     });
     document.getElementById("accionIniciarSesion").addEventListener("click", function () {
         this.style.display = "none";
@@ -25,14 +27,43 @@ window.onload = () => {
         document.getElementById("accionRegistrar").setAttribute("style", "display: inline");
     });
 
+
+    //funciones para el servidor que se envian y se reciben
     var Client = {};
     Client.socket = io.connect();
-    Client.askNewPlayer = function(){
+    Client.askNewPlayer = function () {
         Client.socket.emit('newplayer');
     };
-    Client.iniciarSesion = function(nick, cont){
-        Client.socket.emit('iniciarSesion', {nick:"isayenko", cont:1234});
+    Client.iniciarSesion = function (nick, cont) {
+        Client.socket.emit('iniciarSesion', { nick: nick, cont: cont });
     }
+
+    Client.registrarse = function (nick, cont) {
+        Client.socket.emit('registrarse', { nick: nick, cont: cont });
+    }
+
+    Client.socket.on('malIniciado', function () {
+        document.getElementById("textErrorI").innerHTML = "El correo o la contraseña no son correctos. Tal vez no estas registrado";
+        document.getElementById("textErrorI").style.transition = "0.5s";
+        document.getElementById("textErrorI").style.color = "tomato";
+        document.getElementById("invitado").style.marginTop = "120px";
+        setTimeout(function () {
+            document.getElementById("textErrorI").innerHTML = "";
+            document.getElementById("invitado").style.marginTop = "200px";
+        }, 5000);
+    });
+
+    Client.socket.on('nickExiste', function () {
+        document.getElementById("textErrorR").innerHTML = "Este nick ya esta registrado";
+        document.getElementById("textErrorR").style.transition = "0.5s";
+        document.getElementById("textErrorR").style.color = "tomato";
+        document.getElementById("invitado").style.marginTop = "150px";
+        document.getElementsByName("usernameR")[0].focus();
+        setTimeout(function () {
+            document.getElementById("textErrorR").innerHTML = "";
+            document.getElementById("invitado").style.marginTop = "200px";
+        }, 3000);
+    });
 
     Client.askNewPlayer();
 }
--- server.js
+++ server.js
@@ -17,51 +17,110 @@ io.on('connection', function (socket) {
   socket.on('newplayer', function () {
     console.log("Usuario nuevo");
   });
-  socket.on('iniciarSesion', function (data) {
-    iniciarSesion(data)
-  });
+  socket.on('iniciarSesion', iniciarSesion);
   socket.on('registrarse', registrarse);
   socket.on('jugarinvitado', jugarinvitado);
 });
 
 
+
+
+//funciones personales
 function iniciarSesion(data) {
-  console.log("Iniciar Sesion");
-  console.log(data.nick);
-  console.log(data.cont);
+  consultarUsuarioRegistrado(data.nick, data.cont);
 }
 
 function registrarse(data) {
   insertarMongo(data.nick, data.cont);
-  console.log("registrarse");
 }
 
 function jugarinvitado() {
   console.log("jugarinvitado");
 }
 
+//funcion que redirecciona al juego
+function redireccionarJuego(){
+  console.log("redireccionando al juego");
+}
+
+
+//funciones de consultas de mongo
 function conexioMongo() {
   var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
-
   // Connection URL
   var url = 'mongodb://localhost:27017/projecte';
-
   // Use connect method to connect to the server
   MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-    return db;
+  });
+}
+
+function conexionMongo() {
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/";
+  return { var: MongoClient, direccion: url };
+}
+
+function consultaMongo(dbo, nick, contr = false) {
+  var query;
+  //si no hay contraseña es la consulta de registro
+  if(!contr){
+    console.log("consulat de registro");
+    query = { nickname: nick };
+    //si hay contraseña es una consulta que verifica si el usuario esta registrado
+  }else{
+    console.log("consulat de incio");
+    query = { nickname: nick,  contrasenya:contr};
+  }
+  return new Promise(function (resolve, reject) {
+    dbo.collection("usuaris").find(query).toArray(function (err, result) {
+      if (err) throw err;
+      resultado = result.length == 0 ? false : true;
+      resolve(resultado);
+    });
   });
   
 }
 
-function insertarMongo(nick, contr) {
-  var db = conexioMongo();
-  myobj = { nickName: nick, contrasenya: contr };
-  dbo.collection("usuarios").insertOne(myobj, function (err, res) {
+function consultarUsuarioRegistrado(nick, contr){
+  conexionMongo().var.connect(conexionMongo().direccion, function (err, db) {
     if (err) throw err;
-    console.log("usuario registrado");
-    db.close();
+    var dbo = db.db("projecte");
+    consultaMongo(dbo, nick, contr).then(function (existe) {
+      var myobj = { nickname: nick, contrasenya: contr };
+      if (existe) {
+        dbo.collection("usuaris").insertOne(myobj, function (err, res) {
+          if (err) throw err;
+          redireccionarJuego();
+          db.close();
+        });
+      }else{
+        console.log("este usuario no esta registrado");
+        io.emit('malIniciado');
+      }
+    }).catch(console.log);
+  });
+}
+
+function insertarMongo(nick, contr) {
+  conexionMongo().var.connect(conexionMongo().direccion, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("projecte");
+    consultaMongo(dbo, nick).then(function (existe) {
+      var myobj = { nickname: nick, contrasenya: contr };
+      if (!existe) {
+        dbo.collection("usuaris").insertOne(myobj, function (err, res) {
+          if (err) throw err;
+          console.log("nou usuari registrat");
+          redireccionarJuego();
+          db.close();
+        });
+      }else{
+        console.log("este usuario ya existe");
+        io.emit('nickExiste');
+      }
+    }).catch(console.log);
   });
 }
