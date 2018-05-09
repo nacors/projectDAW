de28ab8d103b60eb7f187be734287d9d83b91754
ivanisayenko <ivanisayenko@hotmail.es>
2018-05-09 10:34:57 +0200

    tileset.tsx corregido

--- media/mapas/tileset.tsx
+++ media/mapas/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tileset" tilewidth="32" tileheight="32" tilecount="232" columns="29">
- <image source="../../../Baixades/Free Pixel Art plataformer painted style/PNG/tileset.png" width="950" height="256"/>
+ <image source="../imagenes/escenarios/montaña/tileset.png" width="950" height="256"/>
 </tileset>
