d45458e6ac718e48a8c037fd29dc6945202ed94d
ivanisayenko <ivanisayenko@hotmail.es>
2018-05-09 13:27:30 +0200

    push imagenes

---
+++ media/mapas/Textures&trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Textures&amp;trees" tilewidth="16" tileheight="16" tilecount="255" columns="17">
+ <image source="../mapa2 imagenes/PNG/Textures&amp;trees.png" width="272" height="240"/>
+</tileset>
