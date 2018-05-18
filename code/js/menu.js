330b2910cd4ed692ba088066b9f83adfe7817ae7
ivanisayenko <ivanisayenko@hotmail.es>
2018-05-18 17:30:51 +0200

    rediseño

--- code/js/menu.js
+++ code/js/menu.js
@@ -24,8 +24,6 @@ window.onload = () => {
         info.innerHTML += "<a id='noSalir' href=''>Cancelar</a>";
         document.getElementById("noSalir").addEventListener("click", function (e) {
             e.preventDefault();
-            document.getElementById("jugarJuego").style.background = "#6c5ce7";
-            document.getElementById("cerrarSesion").style.background = "#a29bfe";
             info.innerHTML = "<h3>Atención, el emparejamiento es aleatorio. Para buscar una sesión presiona el botón de abajo</h3><br>";
             info.innerHTML += "<div id='imagenJuego'></div>";
             info.innerHTML += "<a id='enlacePartidaJ' href='/jugar'>JUGAR</a>";
@@ -37,7 +35,6 @@ window.onload = () => {
         document.getElementById("salir").addEventListener("click", function (e) {
             sessionStorage.setItem('usuario', "invitado");
         });
-
     });
 
     document.getElementById("clasificacion").addEventListener("click", function () {
